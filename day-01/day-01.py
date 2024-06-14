ed72cbf6f79b8cb882d0d39b71b4571451b77917
“nkadiri09@gmail.com” <nkadiri09@gmail.com>
2024-06-14 12:02:10 -0400

    first commit

---
+++ day-01/day-01.py
@@ -0,0 +1,4 @@
+print('Welcome to the banner name generator')
+city = input("what's the name of city name you grow up?")
+petname = input("what is your pet name")
+print("your bamd name could be"+city+petname)
